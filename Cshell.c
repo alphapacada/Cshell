1a1ad158499a077d9c7f7fa66076bd98d452e9ea
arpacada <arpacada@up.edu.ph>
2017-12-07 06:12:06 +0800

    stuff

--- Cshell.c
+++ Cshell.c
@@ -8,9 +8,11 @@
 #include <windows.h>
 #include <time.h>
 #include <dos.h>
+#include <sys/stat.h>
 #define getCurrDir _getcwd
 #define WIN32_LEAN_AND_MEAN
 #include <stdint.h> // portable: uint64_t   MSVC: __int64
+
 /*
 #ifdef _LIBC
 
@@ -23,14 +25,17 @@ static char* const ab_weekday_name[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri"
 
 
 bool getCommand();
+char* directory();
 void splitTokens(char string[], char* array[], int* numOfTokens, char delim[]);
 int makeDirectory(char* path);
 int changeDirectory(char* path);
+int deleteFile(char* path);
 
 
 
-const char *wrongSyntax = "/nThe syntax of the command is incorrect.";
-const char *wrongDate = "/nThe system cannot accept the date entered.";
+const char *wrongSyntax = "\nThe syntax of the command is incorrect.";
+const char *wrongDate = "\nThe system cannot accept the date entered.";
+const char *missingFile = "\nThe system cannot find the file specified.";
 
 char buff[FILENAME_MAX];
 int main() {
@@ -38,7 +43,6 @@ int main() {
     bool run = true;
 
     while( run ) {
-        printf("loop");
         directory();
         getCommand();
     }
@@ -71,7 +75,7 @@ int gettimeofday(struct timevalue * tp, struct timezone * tzp)
     return 0;
 }
 void splitTokens(char* string, char* array[], int* numOfTokens, char delim[]) {
-printf("SP");
+
     char *token;
     int i  = 0;
     token = strtok(string, delim);
@@ -83,16 +87,20 @@ printf("SP");
     }
 
     *numOfTokens = i;
-    printf("num of tokens %d", *numOfTokens);
 }
 
-int directory () {
+char* directory () {
 
   getCurrDir(buff, FILENAME_MAX );
-  printf("%s>\n", buff);
-  return 1;
+  printf("%s>", buff);
+  return buff;
 
   }
+char* getDirectory() {
+    getCurrDir(buff, FILENAME_MAX );
+    return buff;
+
+}
 
 int makeDirectory(char* path) {
 
@@ -114,24 +122,24 @@ int changeDirectory(char* path) {
 
 int date() {
 
-time_t t = time(NULL);
-struct tm tm = *localtime(&t);
-//int month;
-int day;
-int year;
-char weekday[20], month[20];
-//char buffer[100];
-char *dateTokens[5] = {NULL};
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    //int month;
+    int day;
+    int year;
+    char weekday[20], month[20];
+    //char buffer[100];
+    char *dateTokens[5] = {NULL};
 
-int numOfTokens = 0;
-printf("The current date is: %s %d/%02d/%d\n", ab_weekday_name[tm.tm_wday], tm.tm_mon + 1, tm.tm_mday,   tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
-printf("Enter the new date: (mm-dd-yy)");
-char ch;
-//while ((ch=getchar()) != EOF && ch != '\n');
-//scanf("%s", buffer);
-char buffer[] = "2011-02-31";
-splitTokens(buffer, dateTokens, &numOfTokens, " /-");
-return 0;
+    int numOfTokens = 0;
+    printf("The current date is: %s %d/%02d/%d\n", ab_weekday_name[tm.tm_wday], tm.tm_mon + 1, tm.tm_mday,   tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    printf("Enter the new date: (mm-dd-yy)");
+    char ch;
+    //while ((ch=getchar()) != EOF && ch != '\n');
+    //scanf("%s", buffer);
+    char buffer[] = "2011-02-31";
+    splitTokens(buffer, dateTokens, &numOfTokens, " /-");
+    return 0;
 }
 
 int getTime() {
@@ -186,10 +194,258 @@ int clrscr() {
     return 1;
 
 }
+
+long GetAvailableSpace(const char* path)
+{
+    /*
+  struct statvfs stat;
+
+  if (statvfs(path, &stat) != 0) {
+    // error happens, just quits here
+    return -1;
+  }
+
+  // the available size is f_bsize * f_bavail
+  return stat.f_bsize * stat.f_bavail;
+  */
+  return 1;
+}
+int concatStr(char* dest, char* append) {
+
+   for(;*dest!='\0';dest++){
+   }
+   while((*dest++ = *append++)!='\0'){
+   }
+   append='\0';
+   return 0;
+}
+
+int removeDirectory(char *path) {
+    rmdir(path);
+}
+int deleteFile(char* path) {
+    char* dir = getDirectory();
+    char allowDelete = 'Y';
+   // char* pathToDel = strstr(dir, path) != NULL ? path : concatStr(dir,path);
+    if(checkPathType(path) == 1 ) {
+        printf("%s\\*, Are you sure (Y/N)?", directory());
+        scanf("%c", &allowDelete);
+        if(allowDelete == 'Y') {
+            printf("%s", path);
+                removeDirectory(path);
+        }
+
+    } else {
+         if(remove(path) == 0)
+		  printf("File %s  deleted.\n", path);
+		else
+		  fprintf(stderr, "Error deleting the file %s.\n", path);
+    }
+
+
+
+    return 0;
+}
+//returns 1 if it is a path to a folder
+//returns 0 if it is a path to a file
+int checkPathType(char* path) {
+    struct stat s;
+    if( stat(path,&s) == 0 )
+    {
+        if( s.st_mode & S_IFDIR )
+        {
+            //it's a directory
+            return 1;
+
+        }
+        else if( s.st_mode & S_IFREG )
+        {
+            //it's a file
+            return 0;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        //error
+        return -1;
+    }
+}
+int dir (char* path) {
+    char *starting_dir = getcwd(NULL,0);
+
+    if(path != NULL){
+        chdir(path);
+    }
+
+    DIR *dir;
+    struct dirent *ent;
+    char *current_dir = getcwd(NULL,0);
+    struct stat st;
+    int size;
+    int file_count = 0;
+    int dir_count = 0;
+    struct tm *time_;
+    char buf[100];
+
+
+    printf("Volume in drive C is Acer.\nVolume Serial Number is E2B3-F876 \n\n  Directory of %s\n\n", current_dir);
+
+    if ((dir = opendir (current_dir)) != NULL) {
+      /* print all the files and directories within directory */
+        while ((ent = readdir(dir)) != NULL) {
+            stat(ent->d_name, &st);
+            time_= localtime(&(st.st_mtime));
+            strftime(buf, sizeof(buf), "%m/%d/%Y  %I:%M %p", time_);
+
+            printf(buf);
+
+            struct stat s;
+
+            if(stat(ent->d_name,&s) == 0){
+                if(s.st_mode & S_IFDIR){ //If path entry is a directory
+                    dir_count++;
+                    printf ("   <DIR>\t%.0d\t%s\n", st.st_size, ent->d_name);
+                }else if( s.st_mode & S_IFREG ){ //If path entry is a regular file
+                    file_count++;
+                    printf ("        \t%d\t%s\n", st.st_size , ent->d_name);
+                }else{
+                    printf("Type not dir nor regular file.");
+                }
+            }else{
+                printf("Error: INVALID TYPE");
+            }
+
+            size = st.st_size + size;
+        }
+
+        printf("\t    %d File(s) \t\t%d bytes \n", file_count, size);
+        printf("\t    %d Dir(s) \t\t%d bytes \n", dir_count, GetAvailableSpace(path));
+        closedir (dir);
+
+    }
+
+    chdir(starting_dir);
+}
+
+int typeFile(char *filepath) {
+    FILE *fptr;
+    char c;
+
+    // Open file
+    fptr = fopen(filepath, "r");
+    if (fptr == NULL)
+    {
+        printf("%s", missingFile);
+        exit(0);
+    }
+
+    // Read contents from file
+    c = fgetc(fptr);
+    while (c != EOF)
+    {
+        printf ("%c", c);
+        c = fgetc(fptr);
+    }
+
+    fclose(fptr);
+    return 0;
+}
+int renameFile(char** filenames) {
+    int ret = 1;
+
+   // printf("%s",filenames[1]);
+   // printf("%s",filenames[2]);
+   if((sizeof(filenames) - 1) % 2 == 0) {
+     return 0;
+   }
+
+   int start = 1;
+   int i = 0 ;
+   for(i = 1; i < sizeof(filenames); i+=2) {
+        //printf("%d", i);
+      ret = rename(filenames[i], filenames[i+1]);
+      if(ret != 0 ) {
+        printf("The system cannot find the file specified.\n");
+      }
+   }
+
+
+   if(ret == 0) {
+      printf("File renamed successfully");
+   } else {
+      printf("Error: unable to rename the file");
+   }
+
+}
+int copyFile(char** token) {
+    char *starting_dir = getcwd(NULL,0);
+
+    char *dirc, *basec, *bname, *dname;
+    char *path = token[2];
+
+    dirc = strdup(path);
+    basec = strdup(path);
+    dname = dirname(dirc);
+    bname = basename(basec);
+
+
+    char *dirc2, *basec2, *bname2, *dname2;
+    char *path2 = token[1];
+
+    dirc2 = strdup(path2);
+    basec2 = strdup(path2);
+    dname2 = dirname(dirc2);
+    bname2 = basename(basec2);
+
+                if(token[1] == NULL || token[2] == NULL) {
+        printf("Too few arguments\n");
+      } else {
+        char ch;
+        FILE *source, *target;
+        chdir(dname2);
+        source = fopen(bname2, "r");
+        if(source == NULL) {
+          printf("Source file cannot be found.\n");
+        } else {
+          if(strcmp(token[1], token[2]) == 0) {
+            printf("Source file must not be same with target file.\n");
+          }
+           else {
+                chdir(dname);
+            target = fopen(bname, "w");
+            ch = fgetc(source);
+            while (ch != EOF) {
+                fputc(ch, target);
+                ch = fgetc(source);
+            }
+            fclose(target);
+          }
+           printf("Successful Copy!\n\n");
+          fclose(source);
+          chdir(starting_dir);
+        }
+      }
+}
+int count(char** array) {
+    int numberofelements = 0;
+    int i;
+   // printf("%d ", sizeof(array));
+    for(i=0; i < sizeof(array); i++) {
+
+   if(array[i] != NULL)    // If pointer is NULL then increase the elements
+      ++numberofelements;
+    }
+   // printf("%d", numberofelements);
+    return numberofelements;
+}
 bool getCommand() {
     int MAX_SIZE = 100;
     char str[MAX_SIZE];
-    scanf ("%[^\n]%*c", str);
+     scanf ("%[^\n]%*c", str);
     char *array[5] = {NULL};
 
     int numOfTokens = 0;
@@ -224,12 +480,53 @@ bool getCommand() {
         if(strcmp (array[0], "time") == 0) {
             getTime();
         }
+        if(strcmp (array[0], "dir") == 0) {
+            dir(array[1]);
+        }
         if(strcmp (array[0], "exit") == 0) {
             return false;
         }
+        if(strcmp (array[0], "del") == 0) {
+            deleteFile(array[1]);
+        }
+        if(strcmp (array[0], "rename") == 0) {
+
+        }
+        if(strcmp (array[0], "type") == 0) {
+            typeFile(array[1]);
+        }
+        if(strcmp (array[0], "rmdir") == 0) {
+            removeDirectory(array[1]);
+        }
+         if(strcmp (array[0], "cd..") == 0) {
+            changeDirectory("..");
+        }
+        if(strcmp (array[0], "cd") == 0) {
+            int n = count(array);
+            if( n == 1) {
+                printf("%s\n\n", getDirectory());
+            } else {
+                chdir(array[1]);
+            }
+
+        }
+        if(strcmp (array[0], "cd/") == 0 || strcmp (array[0], "cd\\") == 0 ) {
+            changeDirectory("/");
+        }
+
+        if(strcmp (array[0], "rename") == 0) {
+            renameFile(array);
+        }
+
+        if(strcmp (array[0], "copy") == 0) {
+            copyFile(array);
+        }
+
 
     }
     free(str);
+    char ch;
+    //while ((ch=getchar()) != EOF && ch != '\n');
     return true;
 }
 
